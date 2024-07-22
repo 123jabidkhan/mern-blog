9c3ec511ce11f1d5283b1cb323a3efe653c376f0
jabidkhan <jabidkhanchotu1996@gmail.com>
2024-07-22 18:01:37 +0530

    Mongodb database connected

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+// mongodb connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+console.log('Mongodb is connected!');
+}).catch(err=>{
+    console.log('MOngodb connection error >>', err);
+})
 
 app.get('/',(req, res)=>{
     return res.send('data from server!!');
 })
 
+// PORT listening at 8001
 const PORT  = 8001;
 app.listen(PORT,(req, res)=>{
     console.log(`Server running at PORT = ${PORT}`);
