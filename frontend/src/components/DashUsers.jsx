1ac1823e6979d882ac0f0a181dae6e3efb331f20
jabidkhan <jabidkhanchotu1996@gmail.com>
2024-08-03 17:55:40 +0530

    send comment UI designed with functionalitys

--- frontend/src/components/DashUsers.jsx
+++ frontend/src/components/DashUsers.jsx
@@ -1,7 +1,6 @@
 import { Modal, Table, Button, Checkbox } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import {
   HiOutlineExclamationCircle,
   HiOutlineTrash,
