6a40dc202c5782349a6f6e3875eda3fd83ee94a9
jabidkhan <jabidkhanchotu1996@gmail.com>
2024-08-10 20:23:37 +0530

    About section done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,39 @@
-const About = () => {
+const  About = () =>{
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Jabid`s Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Welcome to Jabid`s BlogHub`, where we believe that you`ll find weekly articles and
+             tutorials on topics such as web development, software engineering, and programming languages. 
+             Whether you`re here to learn, get inspired,
+             or simply enjoy some well-crafted content, you`ve come to the right 
+             place.
+            </p>
+
+            <p>
+            Our Mission
+            At BlogHub, our mission is to 
+            We encourage you to leave comments on our posts and engage with
+            other readers. We strive to provide that is 
+             informative, entertaining, insightful
+            to help our readers grow in their careers , find inspiration, stay informed.
+            </p>
+
+            <p>
+            We are constantly updating our content to ensure that it remains fresh,
+             relevant, and valuable to our readers. Whether you’re a tech enthusiast, food lover, small business owner,
+              there’s something here for everyone.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
