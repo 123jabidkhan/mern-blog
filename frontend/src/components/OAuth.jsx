4dea74b13f37544534b331cb032d3860b99b0479
jabidkhan <jabidkhanchotu1996@gmail.com>
2024-08-11 10:49:42 +0530

    Signup section designed changes fixed

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -38,9 +38,38 @@ export default function OAuth() {
         }
     } 
   return (
-    <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
-        <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
-        Continue with Google
-    </Button>
+    // <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+    //     <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
+    //     Continue with Google
+    // </Button>
+    <div className="px-4 pb-2">
+    <button onClick={handleGoogleClick} className="flex items-center justify-center w-full p-3 text-lg font-medium rounded-full bg-white text-gray-700 border border-gray-300 shadow-md hover:bg-gray-100 focus:outline-none">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 48 48"
+        width="24px"
+        height="24px"
+        className="mr-2"
+      >
+        <path
+          fill="#4285F4"
+          d="M 23.5 12.7 c 2.1 0 3.9 0.7 5.4 1.9 l 4-4 c -2.7 -2.4 -6.3 -3.9 -10.4 -3.9 c -5.9 0 -10.8 3.4 -13.2 8.4 l 4.9 3.8 c 1.3 -3.6 4.7 -6.2 8.3 -6.2 Z"
+        />
+        <path
+          fill="#34A853"
+          d="M 12.1 25.3 c -0.4 -1.1 -0.7 -2.2 -0.7 -3.3 c 0 -1.2 0.3 -2.3 0.7 -3.3 l -4.9 -3.8 c -1.3 2.4 -2.1 5.1 -2.1 8.1 c 0 3 0.8 5.7 2.1 8.1 Z"
+        />
+        <path
+          fill="#FBBC05"
+          d="M 23.5 35.3 c -3.7 0 -7 -2.1 -8.7 -5.2 l -4.9 3.8 c 2.4 5 7.3 8.4 13.2 8.4 c 3.8 0 7.2 -1.3 9.9 -3.5 l -4.8 -3.8 c -1.3 0.9 -3 1.5 -4.7 1.5 Z"
+        />
+        <path
+          fill="#EA4335"
+          d="M 44 24.5 c 0 -1.5 -0.2 -2.9 -0.5 -4.3 H 23.5 v 8.1 h 11.6 c -0.5 2.2 -1.8 4.1 -3.7 5.4 l 4.8 3.8 c 2.8 -2.6 4.5 -6.5 4.5 -11 Z"
+        />
+      </svg>
+      Sign in with Google
+    </button>
+  </div>
   )
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,26 +1,174 @@
+// import { useState } from "react";
+// import { Button, Spinner } from "flowbite-react";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { useDispatch,useSelector } from "react-redux";
+// import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+// import OAuth from "../components/OAuth";
+
+// const SignUp = () => {
+//   const [formData, setFormData] = useState({});
+//   // const [loading, setLoading] = useState(false);
+//   // const [errorMessage, setErrorMessage] = useState(null);
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const  {loading, error:errorMessage} = useSelector((state)=>state.user);
+
+//   // input on change
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   // form submit
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if (!formData.username || !formData.email || !formData.password) {
+//       return dispatch(signInFailure("All fields are required."));
+//     }
+//     try {
+//       // ..without Redux..
+//       // setLoading(true);
+//       // setErrorMessage(null);
+
+//       // wih Redux, loading start and error value set null:
+//       dispatch(signInStart());
+//       const res = await axios.post("/api/auth/signup", formData);
+//       if (res.data.success === false) {
+//         return dispatch(signInFailure(res.data.message));
+//       }
+//       // if response ok
+//       if (res.statusText) {
+//         dispatch(signInSuccess(res.data.message));
+//         navigate("/sign-in");
+//       }
+//       console.log(res.data);
+//     } catch (error) {
+//       dispatch(signInFailure(error.response.data.message));
+//     }
+//   };
+
+//   return (
+//     <div className="h-[100vh] items-center flex justify-center lg:px-0">
+//       <div className="max-w-screen-xl bg-white border shadow sm:rounded-lg flex justify-center flex-1">
+//         <div className="flex-1 bg-gradient-to-r from-pink-500  text-center hidden md:flex">
+//           <div
+//             className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+//             style={{
+//               backgroundImage: `url(https://www.tailwindtap.com/assets/common/marketing.svg)`,
+//             }}
+//           ></div>
+//         </div>
+//         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 bg-white dark:bg-gray-800">
+//           <div className=" flex flex-col items-center">
+//             <div className="text-center">
+//               <h1 className="text-2xl xl:text-4xl font-extrabold bg-gradient-to-r from-pink-400  to-orange-500 bg-clip-text text-transparent">
+//                 Sign Up
+//               </h1>
+//               <p className="text-[14px] text-gray-500">
+//                 Hey enter your details to create your account
+//               </p>
+//             </div>
+
+//             <div className="w-full flex-1 mt-8">
+//               <form onSubmit={handleSubmit}>
+//                 <div className="mx-auto max-w-xs flex flex-col gap-4">
+//                   <input
+//                     className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+//                     type="text"
+//                     placeholder="Enter username"
+//                     name="username"
+//                     onChange={handleChange}
+//                     value={formData.username}
+//                   />
+//                   <input
+//                     className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+//                     type="email"
+//                     placeholder="name@company.com"
+//                     name="email"
+//                     onChange={handleChange}
+//                     value={formData.email}
+//                   />
+//                   <input
+//                     className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+//                     type="password"
+//                     placeholder="Password"
+//                     name="password"
+//                     onChange={handleChange}
+//                     value={formData.password}
+//                   />
+//                   <Button
+//                     type="submit"
+//                     gradientDuoTone="pinkToOrange"
+//                     className="text-white focus:shadow-outline focus:outline-none"
+//                   >
+//                     <span className="ml-3 text-light-100">
+//                       {loading ? (
+//                         <>
+//                           <Spinner size="sm" />
+//                           <span className="pl-3">Loading...</span>
+//                         </>
+//                       ) : (
+//                         "Sign Up"
+//                       )}
+//                     </span>
+//                   </Button>
+//                   <div>
+//                     {errorMessage && (
+//                       <span className="text-red-500 text-sm">
+//                         {errorMessage}
+//                       </span>
+//                     )}
+//                   </div>
+//                   {/* create account with google and loggin */}
+//                   <OAuth/>
+//                   <p className="mt-6 text-sm text-gray-600 text-center">
+//                     Already have an account?{" "}
+//                     <Link to="/sign-in" className="text-blue-500">
+//                       Sign In
+//                     </Link>
+//                   </p>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default SignUp;
+
+// ========================== NEW SIGNUP DESIGN ============================
+
+// new signup design..
 import { useState } from "react";
-import { Button, Spinner } from "flowbite-react";
+import { Spinner } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch,useSelector } from "react-redux";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 import OAuth from "../components/OAuth";
+import loginBg from "../assets/images/loginBg.jpg";
+
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
-  // const [loading, setLoading] = useState(false);
-  // const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const  {loading, error:errorMessage} = useSelector((state)=>state.user);
+    // const [loading, setLoading] = useState(false);
+    // const [errorMessage, setErrorMessage] = useState(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const  {loading, error:errorMessage} = useSelector((state)=>state.user);
 
-  // input on change
+
+    
+//   // input on change
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  // form submit
+//   // form submit
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
@@ -36,7 +184,7 @@ const SignUp = () => {
       const res = await axios.post("/api/auth/signup", formData);
       if (res.data.success === false) {
         return dispatch(signInFailure(res.data.message));
-      }      
+      }
       // if response ok
       if (res.statusText) {
         dispatch(signInSuccess(res.data.message));
@@ -48,93 +196,122 @@ const SignUp = () => {
     }
   };
 
+  const backgroundImage = {
+    backgroundImage: `url(${loginBg})`,
+  };
   return (
-    <div className="h-[100vh] items-center flex justify-center lg:px-0">
-      <div className="max-w-screen-xl bg-white border shadow sm:rounded-lg flex justify-center flex-1">
-        <div className="flex-1 bg-gradient-to-r from-pink-500  text-center hidden md:flex">
-          <div
-            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-            style={{
-              backgroundImage: `url(https://www.tailwindtap.com/assets/common/marketing.svg)`,
-            }}
-          ></div>
+    <>
+      <section className="min-h-screen flex items-stretch text-white ">
+        <div
+          className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center"
+          style={backgroundImage}
+        >
+          <div className="absolute bg-[#008a008f] opacity-85 inset-0 z-0"></div>
+          <div className="w-full px-24 z-10">
+            <h1 className="text-5xl font-bold text-left tracking-wide">
+              Keep it special
+            </h1>
+            <p className="text-3xl my-4">
+              Capture your personal memory in unique way, anywhere.
+            </p>
+          </div>
         </div>
-        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 bg-white dark:bg-gray-800">
-          <div className=" flex flex-col items-center">
-            <div className="text-center">
-              <h1 className="text-2xl xl:text-4xl font-extrabold bg-gradient-to-r from-pink-400  to-orange-500 bg-clip-text text-transparent">
-                Sign Up
-              </h1>
-              <p className="text-[14px] text-gray-500">
-                Hey enter your details to create your account
-              </p>
-            </div>
+        <div
+          className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0"
+          style={{ backgroundColor:'rgb(0 138 0 / 29%)'}}
+        >
+          <div
+            className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center"
+            style={backgroundImage}
+          >
+            <div className="absolute bg-[#28ee49b0] opacity-80 inset-0 z-0"></div>
+          </div>
+          <div className="w-full py-4 z-20">
+            <h1 className="">
+              <svg width="300" height="100" xmlns="http://www.w3.org/2000/svg">
+                <text
+                  x="78%"
+                  y="50%"
+                  fontWeight="bold"
+                  dominantBaseline="middle"
+                  textAnchor="middle"
+                  fontSize="35px"
+                  fill="#00000"
+                >
+                  Sign Up
+                </text>
+              </svg>
+            </h1>
 
-            <div className="w-full flex-1 mt-8">
-              <form onSubmit={handleSubmit}>
-                <div className="mx-auto max-w-xs flex flex-col gap-4">
-                  <input
-                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+            <p className="text-gray-100">or use email your account</p>
+            <form action="" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto"onSubmit={handleSubmit}>
+            <div className="pb-2 pt-4">
+            <input
+                    className="w-full px-5 text-[black] py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                     type="text"
                     placeholder="Enter username"
                     name="username"
                     onChange={handleChange}
-                    value={formData.username}
+                  value={formData.username}
                   />
-                  <input
-                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+              </div>
+              <div className="pb-2 pt-4">
+              <input
+                    className="w-full px-5 py-3 text-[black] rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                     type="email"
                     placeholder="name@company.com"
                     name="email"
                     onChange={handleChange}
                     value={formData.email}
                   />
-                  <input
-                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+              </div>
+              <div className="pb-2 pt-4">
+              <input
+                    className="w-full px-5 py-3 text-[black] rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                     type="password"
                     placeholder="Password"
                     name="password"
                     onChange={handleChange}
                     value={formData.password}
                   />
-                  <Button
-                    type="submit"
-                    gradientDuoTone="pinkToOrange"
-                    className="text-white focus:shadow-outline focus:outline-none"
-                  >
-                    <span className="ml-3 text-light-100">
+              </div>
+
+              <div className="px-4 pb-2 pt-4">
+                <button type="submit" className="uppercase block w-full p-3 text-lg rounded-full bg-[#d30617] hover:bg-[#eb3f4d] focus:outline-none">
+                <span className="ml-3 text-light-100">
                       {loading ? (
-                        <>
-                          <Spinner size="sm" />
-                          <span className="pl-3">Loading...</span>
-                        </>
-                      ) : (
-                        "Sign Up"
-                      )}
-                    </span>
-                  </Button>
-                  <div>
-                    {errorMessage && (
-                      <span className="text-red-500 text-sm">
-                        {errorMessage}
-                      </span>
-                    )}
-                  </div>
-                  {/* create account with google and loggin */}
-                  <OAuth/>
-                  <p className="mt-6 text-sm text-gray-600 text-center">
-                    Already have an account?{" "}
-                    <Link to="/sign-in" className="text-blue-500">
-                      Sign In
-                    </Link>
-                  </p>
-                </div>
-              </form>
-            </div>
+                         <>
+                           <Spinner size="sm" />
+                           <span className="pl-3">Loading...</span>
+                         </>
+                       ) : (
+                         "Sign Up"
+                       )}
+                     </span>
+                </button>
+              </div>
+              <div>
+                     {errorMessage && (
+                       <span className="text-red-500 text-sm">
+                         {errorMessage}
+                       </span>
+                     )}
+                   </div>
+              {/* signup wih google button */}
+              <OAuth/>
+              <p className="mt-6 text-sm text-gray-600 text-center">
+                     Already have an account?{" "}
+                     <Link to="/sign-in" className="text-blue-500">
+                       Sign In
+                     </Link>
+                   </p>
+              <div className="p-4 text-center right-0 left-0 flex justify-center space-x-4 mt-16 lg:hidden "></div>
+            </form>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
+
 export default SignUp;
