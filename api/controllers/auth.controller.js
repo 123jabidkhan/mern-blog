40cc40c43e597226bdfdb85a6204da1328411147
jabidkhan <jabidkhanchotu1996@gmail.com>
2024-08-17 00:01:45 +0530

    google auth access token issue fixed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,7 +70,7 @@ const signIn = async (req, res, next) =>{
     const user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign(
-        { id: user._id},
+        { id: user._id, isAdmin:user.isAdmin},
         process.env.JWT_SECRET_KEY
       );
       const { password, ...rest } = user._doc;
